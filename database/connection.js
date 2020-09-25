7b127b5041d0d60f9e85bf62894751f10d3a2894
charlesw25 <woodcharles544@gmail.com>
2020-09-25 17:24:57 -0400

    Able to add entries to MongoDB database #16

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,15 @@
 // Following code adapted from: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
 const {MongoClient} = require('mongodb');
 
+
+
+// Adds a new entry for login. Listings are objects.
+async function addLogin(client, newListing){
+  const result = await client.db("login_db").collection("login_collection").insertOne(newListing);
+  console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+
 // Retrieves a list of databases in the cluster and prints the results.
 async function listDatabases(client){
   databasesList = await client.db().admin().listDatabases();
@@ -8,6 +17,7 @@ async function listDatabases(client){
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
 
 async function main(){
   /**
@@ -17,13 +27,14 @@ async function main(){
   const uri = "mongodb+srv://cse442:a9-sleeper@cluster0.s9pn2.mongodb.net/login_db?retryWrites=true&w=majority";
 
   // const client = new MongoClient(uri);
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }) // No warnings
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); // No warnings
   await client.connect();
   await listDatabases(client);
 
   try {
       await client.connect(); // Connect to MongoDB cluster
       await listDatabases(client); // Make DB calls
+      // await addLogin(client, { username: "test_username", password: "test_password"}) // Create test login
 
   } catch (e) {
       console.error(e);
