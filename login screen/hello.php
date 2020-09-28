5350c759083feee6bc2eafc7bd07fde422679594
smalbec <smalbec@buffalo.edu>
2020-09-28 18:54:15 -0400

    Reorganized HTML, added basic php implementation

---
+++ login screen/hello.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
+<a href="#" class="item-link list-button">Sign In</a>
+<html>
+<body>
+
+Welcome <?php echo $_POST["username"]; ?><br>
+Your email address is: <?php echo $_POST["password"]; ?>
+
+</body>
+</html> 
