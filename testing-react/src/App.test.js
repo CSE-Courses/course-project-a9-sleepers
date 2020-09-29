397ccbabf6f56b88515f5cf0efd60f50e8aac86d
smalbec <smalbec@buffalo.edu>
2020-09-29 19:19:57 -0400

    Added integration tests for express and express validation when using it on login screen #20

--- testing-react/src/App.test.js
+++ testing-react/src/App.test.js
@@ -2,24 +2,76 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import Account from "./Account";
 import App from "./App";
+import server from "./server";
 import toJson from "enzyme-to-json";
 
-it("renders correctly", () => {
-  const wrapper = mount(<App />);
-  expect(wrapper.state("error")).toEqual(null);
+const express = require('express');
+const { check } = require('express-validator');
+const request = require('supertest');
+
+const app = express();
+
+app.post('/submit-student-data', [
+  check('username','Username must be AlphaNumerical').isAlphanumeric(),
+  check('username','Username cannot be empty').notEmpty(),
+  check('password','Password must be AlphaNumerical').isAlphanumeric(),
+  check('password','Password cannot be empty').notEmpty()
+], (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
 });
 
 
- it("renders without crashing", () => {
-  shallow(<App />);
+it('fails if invalid username or password are passed', done => {
+	request(app)
+		.post('/submit-student-data')
+		.send({ username: '', password: '[]' })
+		.expect(500, done);
 });
 
-it("renders Account header", () => {
-  const wrapper = shallow(<App />);
-  const header = <h1>Display Active Users Account Details</h1>;
-  // expect(wrapper.contains(welcome)).toBe(true);
-  expect(wrapper.contains(header)).toEqual(true);
-}); 
+it('passes if valid username or password are passed', done => {
+	request(app)
+		.post('/submit-student-data')
+		.send({ username: 'Jonh Doe', password: 'hunter2' })
+		.expect(500, done);
+});
+
+it('fails if valid username but invalid password are passed', done => {
+	request(app)
+		.post('/submit-student-data')
+		.send({ username: 'Jonh Doe', password: '' })
+		.expect(500, done);
+});
+
+it('fails if invalid username but valid password are passed', done => {
+	request(app)
+		.post('/submit-student-data')
+		.send({ username: '\[cool guy]/', password: 'password123' })
+		.expect(500, done);
+});
+
+// it("renders correctly", () => {
+//   const wrapper = mount(<App />);
+//   expect(wrapper.state("error")).toEqual(null);
+// });
+
+
+//  it("renders without crashing", () => {
+//   shallow(<App />);
+// });
+
+// it("renders Account header", () => {
+//   const wrapper = shallow(<App />);
+//   const header = <h1>Display Active Users Account Details</h1>;
+//   // expect(wrapper.contains(welcome)).toBe(true);
+//   expect(wrapper.contains(header)).toEqual(true);
+// }); 
 
 /*
 const user = {
