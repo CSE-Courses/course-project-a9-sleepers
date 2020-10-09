0c748cd819edc503a506a6f7696967f445b660c0
charlesw25 <woodcharles544@gmail.com>
2020-10-08 20:38:59 -0400

    Slight file refactor #35.

--- visual-desktop-tools-box/src/App.js
+++ visual-desktop-tools-box/src/App.js
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import MainPage from './components/MainPage/MainPage';
 import Calculator from './components/Widgets/Calculator/Calculator';
+import Countdown from './components/Widgets/Countdown';
+import Stopwatch from './components/Widgets/Stopwatch';
 
 
 class App extends Component {
@@ -16,12 +18,14 @@ class App extends Component {
      <BrowserRouter>
        <Switch>
 
-          <Route path = "/" exact component = {MainPage}/>
+          <Route path = "/" exact component = {MainPage}/> 
 
+          <Route path = "/Calculator" exact component = {Calculator}/> 
+        
+          <Route path = "/Timer" exact component = {Countdown}/>
 
-
-
-
+          <Route path = "/Stopwatch" exact component = {Stopwatch}/>
+         
 
        </Switch>
      </BrowserRouter>
