c331d54b9ca8d1521891a4eb4f58215573b5af41
charlesw25 <woodcharles544@gmail.com>
2020-10-11 22:07:48 -0400

    Cleaned up JSX for Login.js #35

--- visual-desktop-tools-box/src/components/Login/Login.js
+++ visual-desktop-tools-box/src/components/Login/Login.js
@@ -6,12 +6,11 @@ export default function MainPage (){
   return(
     <div>
         <script src={'./database/connection.js'}></script>
-        <form form onSubmit="getFormData()">
+        <form onSubmit={"getFormData()"}>
             Username: <input name="username" type="text" placeholder="Username" /> <br />
             Password: <input name="password" type="password" placeholder="Password"/> <br />
             <input type="submit" />
         </form>
     </div>
     );
-
 }
