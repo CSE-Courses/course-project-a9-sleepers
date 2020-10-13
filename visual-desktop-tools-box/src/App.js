cb8f9e7ed7533f0fba5ad77526bb4b8b33f80a48
Richard <rwu23@buffalo.edu>
2020-10-13 19:16:00 -0400

    add/delete task implementation

--- visual-desktop-tools-box/src/App.js
+++ visual-desktop-tools-box/src/App.js
@@ -9,6 +9,7 @@ import MainPage from './components/MainPage/MainPage';
 import Calculator from './components/Widgets/Calculator/Calculator';
 import Countdown from './components/Widgets/Countdown';
 import Stopwatch from './components/Widgets/Stopwatch';
+import ToDoList from './components/Widgets/ToDoList/ToDoList';
 
 
 class App extends Component {
@@ -25,6 +26,8 @@ class App extends Component {
           <Route path = "/Timer" exact component = {Countdown}/>
 
           <Route path = "/Stopwatch" exact component = {Stopwatch}/>
+
+          <Route path = "/ToDoList" exact component = {ToDoList}/>
          
 
        </Switch>
