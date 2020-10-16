5b0b9de7c0d012c36709b9dac405364a0348e803
smalbec <smalbec@buffalo.edu>
2020-10-16 17:50:18 -0400

    Will now follow desktop format, pressing cat will take you now to its own page

--- src/App.js
+++ src/App.js
@@ -9,41 +9,33 @@ import MainPage from './components/MainPage/MainPage';
 import Calculator from './components/Widgets/Calculator/Calculator';
 import Countdown from './components/Widgets/Countdown';
 import Stopwatch from './components/Widgets/Stopwatch';
+import Cat from './components/Widgets/Cat/Cat';
 
 
 class App extends Component {
 
-  state = {
-    showMessage: false
-  };
-  onButtonClickHandler = () => {
-    this.setState({ showMessage: !this.state.showMessage });
-  };
 
   render(){
   return (
 
-    <div className="App">
-    {this.state.showMessage && <p>Hi</p>}
-    <button onClick={this.onButtonClickHandler}>Enter</button>
-    </div>
+  <Fragment>
+     <BrowserRouter>
+       <Switch>
 
-  // <Fragment>
-  //    <BrowserRouter>
-  //      <Switch>
+          <Route path = "/" exact component = {MainPage}/> 
 
-  //         <Route path = "/" exact component = {MainPage}/> 
-
-  //         <Route path = "/Calculator" exact component = {Calculator}/> 
+          <Route path = "/Calculator" exact component = {Calculator}/> 
         
-  //         <Route path = "/Timer" exact component = {Countdown}/>
+          <Route path = "/Timer" exact component = {Countdown}/>
 
-  //         <Route path = "/Stopwatch" exact component = {Stopwatch}/>
+          <Route path = "/Stopwatch" exact component = {Stopwatch}/>
+
+          <Route path = "/Cat" exact component = {Cat}/>
          
 
-  //      </Switch>
-  //    </BrowserRouter>
-  // </Fragment>
+       </Switch>
+     </BrowserRouter>
+  </Fragment>
   );
 }
 }
