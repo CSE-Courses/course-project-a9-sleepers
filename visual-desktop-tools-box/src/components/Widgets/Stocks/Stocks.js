651024c9629ef3b4d2d99fd29c70fabd1b95e39b
charlesw25 <woodcharles544@gmail.com>
2020-10-29 16:56:29 -0400

    Added line graphs for Stocks frontend #62

--- visual-desktop-tools-box/src/components/Widgets/Stocks/Stocks.js
+++ visual-desktop-tools-box/src/components/Widgets/Stocks/Stocks.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import NavBar from '../../NavBar/NavBar';
-import axios from 'axios';
+// import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+import { Card, Button, Transition, Accordion } from 'react-bootstrap';
 
+const state = {
+  labels: ['9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
+          '13:00', '13:30', '14:00', '14:30', '15:00', '15:30','16:00'],
+  datasets: [
+    {
+      label: 'Price (USD)',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: 'rgba(0,0,0,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+  ]
+}
 
 export default class Stocks extends Component {
   constructor(props) {
     super(props);
+  }
+
+  renderLineChart(name, data) {
+    return (
+      <div>
+        <Accordion defaultActiveKey="0">
+
+          <Card.Title>
+          <Accordion.Toggle as={Button} variant="dark" size="lg" eventKey="0" block>
+            {name}
+          </Accordion.Toggle>
+          </Card.Title>
+
+          <Accordion.Collapse eventKey="0">
+          <Card.Body>
+          <div>
+            <Line
+              data={data}
+              options={{
+                title:{
+                  display:true,
+                  text:'Amount (USD)',
+                  fontSize:18
+                },
+                legend:{
+                  display:false,
+                  position:'right'
+                },
+                responsive: true
+              }}
+            />
+          </div>
+          </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+      </div>
+    );
   }
 
   render() {
@@ -13,26 +67,25 @@ export default class Stocks extends Component {
       <div>
         <NavBar/>
 
-          <h1 class="card-header">Stocks</h1>
+        <Card.Header>Stocks</Card.Header>
 
-          <div class="card-body">
+        <Card.Body>
+          {this.renderLineChart("Microsoft", state)}
 
-            <h3 class="card-title">Microsoft</h3>
+          {this.renderLineChart("Google", state)}
 
-            <h3 class="card-title">Google</h3>
+          {this.renderLineChart("Apple", state)}
 
-            <h3 class="card-title">Apple</h3>
+          {this.renderLineChart("Facebook", state)}
 
-            <h3 class="card-title">Facebook</h3>
+          {this.renderLineChart("IBM", state)}
+        </Card.Body>
 
-            <h3 class="card-title">IBM</h3>
+        <Card.Header>Crypto</Card.Header>
 
-          </div>
-
-          <h1 class="card-header">Crypto.</h1>
-          <div class="card-body">
+        <Card.Body>
             <h3 class="card-title">BTC</h3>
-          </div>
+        </Card.Body>
 
       </div>
     );
