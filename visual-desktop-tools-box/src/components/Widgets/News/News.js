51adcf20da85f4038f8f33d1521397eda7502546
charlesw25 <woodcharles544@gmail.com>
2020-11-11 20:45:25 -0500

    Data collected from backend to frontend #84

--- visual-desktop-tools-box/src/components/Widgets/News/News.js
+++ visual-desktop-tools-box/src/components/Widgets/News/News.js
@@ -8,7 +8,59 @@ export default class News extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      numArticles:  0,
+      names:        [],
+      titles:       [],
+      descriptions: [],
+      imgUrls:      [],
+      articleUrls:  [],
+      published:    [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/news')
+      .then(response => {
+          this.setState({
+            numArticles: response.data.totalResults,
+
+            names: response.data.articles.map(name => name.source.name),
+            titles: response.data.articles.map(title=>title.title),
+            descriptions: response.data.articles.map(desc=>desc.description),
+            imgUrls: response.data.articles.map(imgUrl=>imgUrl.urlToImage),
+            articleUrls: response.data.articles.map(url=>url.url),
+          })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  renderArticle() {
+    return(
+      <Card>
+      <div class="card-horizontal">
+        <Card.Img
+        width={320}
+        height={180}
+        variant="right"
+        src="https://image.cnbcfm.com/api/v1/image/106736363-1602180293828-gettyimages-1263061836-daycare0719.jpeg?v=1602180310"
+        />
+        <Card.Body>
+          <a href="https://www.cnbc.com/2020/11/04/coronavirus-live-updates.html"><Card.Title>Coronavirus live updates: Oxford trial chief sees vaccine results this year; Palantir reportedly in talks to help U.K. contact tracing - CNBC</Card.Title></a>
+          <span class="badge badge-secondary">CNBC</span>
+          <Card.Text>
+            The coronavirus has infected more than 47.5 million people globally as of Wednesday, killing at least 1.21 million so far.
+          </Card.Text>
+        </Card.Body>
+      </div>
+
+      <Card.Footer>
+        <small className="text-muted">Published at: {"2020-11-04T08:20:22Z"}</small>
+      </Card.Footer>
+      </Card>
+    );
   }
 
   renderRow() {
@@ -70,8 +122,7 @@ export default class News extends Component {
     return (
       <div>
         <NavBar/>
-          {this.renderRow()}
-          {this.renderRow()}
+          {this.renderArticle()}
       </div>
     );
   }
